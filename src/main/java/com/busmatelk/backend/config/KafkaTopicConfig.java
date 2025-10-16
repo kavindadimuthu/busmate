7291271769ab6055a19fb8ea37760e01e19773fd
ILakshitha <ilakshitha7921@gmail.com>
2025-10-16 22:54:16 +0530

    kafka config commented

--- src/main/java/com/busmatelk/backend/config/KafkaTopicConfig.java
+++ src/main/java/com/busmatelk/backend/config/KafkaTopicConfig.java
@@ -1,24 +1,24 @@
-package com.busmatelk.backend.config;
-
-import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.TopicBuilder;
-
-@Configuration
-public class KafkaTopicConfig {
-
-    @Value("${spring.kafka.template.default-topic}")
-    private String topicName;
-
-    @Bean
-    public NewTopic requestConductorDetailsTopic() {
-        return TopicBuilder.name(topicName).build();
-    }
-
+//package com.busmatelk.backend.config;
+//
+//import org.apache.kafka.clients.admin.NewTopic;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.kafka.config.TopicBuilder;
+//
+//@Configuration
+//public class KafkaTopicConfig {
+//
+//    @Value("${spring.kafka.template.default-topic}")
+//    private String topicName;
+//
 //    @Bean
-//    public NewTopic responseConductorDetailsTopic() {
-//        return TopicBuilder.name().build();
+//    public NewTopic requestConductorDetailsTopic() {
+//        return TopicBuilder.name(topicName).build();
 //    }
-}
+//
+////    @Bean
+////    public NewTopic responseConductorDetailsTopic() {
+////        return TopicBuilder.name().build();
+////    }
+//}
