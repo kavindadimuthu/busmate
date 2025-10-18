528d437a1d5d89aa89273caa5f0e8029bf5a0a3a
Kavinda dewmith <kavindadewmith@gmail.com>
2025-10-18 16:46:05 +0530

    Refactor CORS configuration to temporarily allow all origins

--- src/main/java/com/busmatelk/backend/config/WebConfig.java
+++ src/main/java/com/busmatelk/backend/config/WebConfig.java
@@ -13,11 +13,11 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                            "http://localhost:3000", 
-                            "https://busmate-web-frontend.vercel.app"
-                        ) // list each frontend separately
-                        // .allowedOrigins("*") // Tempory CORS allowance for all URLs
+                        // .allowedOrigins(
+                        //     "http://localhost:3000", 
+                        //     "https://busmate-web-frontend.vercel.app"
+                        // ) // list each frontend separately
+                        .allowedOrigins("*") // Tempory CORS allowance for all URLs
                         .allowedMethods("*") // GET, POST, PUT, etc.
                         .allowedHeaders("*")
                         .allowCredentials(true);
