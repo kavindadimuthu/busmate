8565604a32111ee62a6a41b0aa7a747c7bfca2c6
ILakshitha <ilakshitha7921@gmail.com>
2025-10-20 14:56:30 +0530

    Add ticket validation and retrieval by passenger ID in TicketController and PaymentServiceIMPL

--- src/main/java/com/busmate/ticketing_service/repository/TicketRepo.java
+++ src/main/java/com/busmate/ticketing_service/repository/TicketRepo.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface TicketRepo extends JpaRepository<Tickets, Integer> {
+public interface TicketRepo extends JpaRepository<Tickets, Long> {
     List<Tickets> findByConductorId(String conductorId);
 
     List<Tickets> findByBusId(String busId);
 
     List<Tickets> findByTripId(String tripId);
+
+    List<Tickets> findByPassengerId(String passengerId);
 }
